86c8656ab4c1663e74f55816b89d9b57dbdee861
Senadheera-eng <lahiru.nirmal.senadeera@gmail.com>
2025-09-19 21:56:49 +0530

    Revert "changing app/build.gradle.kts"
    This reverts commit a5785e582804ab9839d0384bbf03e2528d1fc71d.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,9 +18,6 @@ dependencies {
   // When using the BoM, don't specify versions in Firebase dependencies
   implementation("com.google.firebase:firebase-analytics")
 
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-  implementation("androidx.multidex:multidex:2.0.1")
-
 
   // Add the dependencies for any other desired Firebase products
   // https://firebase.google.com/docs/android/setup#available-libraries
@@ -32,9 +29,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
@@ -50,7 +46,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled = true
     }
 
     buildTypes {
